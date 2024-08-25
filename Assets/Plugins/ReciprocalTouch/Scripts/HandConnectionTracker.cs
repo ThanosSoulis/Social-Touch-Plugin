27eed75c229e37151e5af02232015d8b54fcd05a
Thanos Soulis <athan.soulis@gmail.com>
2024-08-25 12:36:42 +0200

    Hiding the XR tracked hand when the Ultraleap hand tracking is active

--- Assets/Plugins/ReciprocalTouch/Scripts/HandConnectionTracker.cs
+++ Assets/Plugins/ReciprocalTouch/Scripts/HandConnectionTracker.cs
@@ -1,11 +1,14 @@
 using Leap.Unity;
 using Leap.Unity.HandsModule;
 using UnityEngine;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class HandConnectionTracker : HandTransitionBehavior
 {
     private MammothRenderer _mammothRenderer;
     private HandBinder _handBinder;
+    private XRInputModalityManager _xrInputModalityManager;
 
     private void Start()
     {
@@ -14,10 +17,59 @@ public class HandConnectionTracker : HandTransitionBehavior
             Debug.LogWarning("HandBinder is not set - Expect errors on hand tracking status changes");
         _mammothRenderer = GetComponentInParent<MammothRenderer>();
         if(_mammothRenderer == null)
-            Debug.LogWarning("MammothRenderer is not set - Expect errors on hand tracking status changes");
+            Debug.LogWarning("MammothRenderer is not set - Expect errors on hand tracking status changes");        
+        _xrInputModalityManager = GetComponentInParent<XRInputModalityManager>();
+        if(_xrInputModalityManager == null)
+            Debug.LogWarning("XR Input Modality Manager is not set - Expect errors on hand tracking status changes");
     }
 
     protected override void HandReset()
+    {
+        HideXRIHand();
+        MammothRendererActivate();
+    }
+
+    protected override void HandFinish()
+    {
+        ShowXRIHand();
+        MammothRendererDeactivate();
+    }
+
+    private void HideXRIHand()
+    {
+        if(_handBinder == null)
+            return;
+        
+        switch (_handBinder.Chirality)
+        {
+            case Chirality.Left:
+                _xrInputModalityManager.leftHand.SetActive(false);
+                break;    
+                
+            case Chirality.Right:
+                _xrInputModalityManager.rightHand.SetActive(false);
+                break;
+        }
+    }
+
+    private void ShowXRIHand()
+    {
+        if(_handBinder == null)
+            return;
+        
+        switch (_handBinder.Chirality)
+        {
+            case Chirality.Left:
+                _xrInputModalityManager.leftHand.SetActive(true);
+                break;    
+                
+            case Chirality.Right:
+                _xrInputModalityManager.rightHand.SetActive(true);
+                break;
+        }
+    }
+
+    private void MammothRendererActivate()
     {
         if(_handBinder == null)
             return;
@@ -34,8 +86,11 @@ public class HandConnectionTracker : HandTransitionBehavior
         }
     }
 
-    protected override void HandFinish()
+    private void MammothRendererDeactivate()
     {
+        if(_handBinder == null)
+            return;
+        
         switch (_handBinder.Chirality)
         {
             case Chirality.Left:
