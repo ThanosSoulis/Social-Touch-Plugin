b74f42426f059ffb200c08823e5994a8b2a00cf2
Thanos Soulis <thanos.soulis@unity3d.com>
2024-08-31 20:39:02 +0200

    Scene Camera handling

--- Assets/Scripts/NetworkedBehaviour/TeleportPlayersManager.cs
+++ Assets/Scripts/NetworkedBehaviour/TeleportPlayersManager.cs
@@ -5,12 +5,19 @@ using UnityEngine.UIElements;
 
 public class TeleportPlayersManager : MonoBehaviour
 {
+    [Header("User Transforms")]
     public Transform userTransformA, userTransformB;
+
+    [Header("Scene Components")]
+    [SerializeField] private Camera sceneCamera;
     
     private NetworkManager _networkManager;
     private void Start()
     {
         _networkManager = NetworkManager.Singleton;
+        
+        // Disable the Scene camera by default
+        sceneCamera.gameObject.SetActive(false);
     }
     private void Update()
     {
@@ -22,6 +29,9 @@ public class TeleportPlayersManager : MonoBehaviour
         // Only allow the server to teleport players
         if(_networkManager.IsClient)
             return;
+        
+        // Enable the Scene Camera, since we are the server
+        sceneCamera.gameObject.SetActive(true);
         
         foreach (var clientPair in _networkManager.ConnectedClients)
         {
