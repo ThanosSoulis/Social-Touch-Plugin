bee5c6ef88a468ae4deec157ee4099f52aed7a2a
Thanos Soulis <athan.soulis@gmail.com>
2024-09-29 22:37:36 +0200

    Setting up Study Controller buttons

---
+++ Assets/Scripts/SetupStudyController.cs
@@ -0,0 +1,40 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupStudyController : MonoBehaviour
+{
+    [SerializeField] Button[] ParticipantAButtons;
+    [SerializeField] Button[] ParticipantBButtons;
+
+    [SerializeField] Button ClosePanelsButton;
+    private ServerStudyController _serverController;
+    
+    void OnEnable()
+    {
+        _serverController = FindAnyObjectByType<ServerStudyController>();
+        if(_serverController.IsUnityNull())
+        {
+            Debug.LogError("Server Study Controller not found! : Cannot setup Participant Buttons");
+            return;
+        }
+
+
+        SetupParticipantButtons(Participant.A, ParticipantAButtons);
+        SetupParticipantButtons(Participant.B, ParticipantBButtons);
+
+        ClosePanelsButton.onClick.AddListener(()=> {_serverController.CloseAllPanels(Participant.A); _serverController.CloseAllPanels(Participant.B);});
+    }
+
+    private void SetupParticipantButtons(Participant participant, Button[] buttons)
+    {
+        buttons[0].onClick.AddListener(() =>_serverController.PrepareInitiateTouch(EmotionalImage.PositiveValenceLowArousal, participant));
+        buttons[1].onClick.AddListener(() =>_serverController.PrepareInitiateTouch(EmotionalImage.PositiveValenceHighArousal, participant));
+        buttons[2].onClick.AddListener(() =>_serverController.PrepareInitiateTouch(EmotionalImage.NegativaValenceLowArousal, participant));
+        buttons[3].onClick.AddListener(() =>_serverController.PrepareInitiateTouch(EmotionalImage.NegativeValenceHighArousal, participant));
+        buttons[4].onClick.AddListener(() =>_serverController.PrepareReceiveTouch(participant));
+        buttons[5].onClick.AddListener(() =>_serverController.PrepareRespondTouch(participant));
+        buttons[6].onClick.AddListener(() =>_serverController.AssessOutputTouch(participant));
+        buttons[7].onClick.AddListener(() =>_serverController.AssessInputTouch(participant));
+    }
+}
